117225f153d6281de535b4100d4e4d57749e698b
Alejandro <alesetneuf@gmail.com>
2019-05-30 18:33:22 +0100

    Implementada nueva parte de atacar, solo falta que se vea y hacer un bucle

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
+import cazadorImages from '../assets/cazadores/*.svg';
+import pokemonImages from '../assets/pokemons/*.svg';
+
 function numAleatorio (min,max){
     return Math.floor(Math.random() * ((max+1) - min)) + min; 
 }
 
 const Pokemon = class {
-    constructor(vida = numAleatorio(100,150), fotoAleatoria = "assets/pokemons/pokemon" + numAleatorio(1,5) + ".svg" ){
+    constructor(vida = numAleatorio(100,150), fotoAleatoria = pokemonImages["pokemon"+ numAleatorio(1,5)]){
         this.life = vida;
         this.img = fotoAleatoria; 
     }
@@ -12,25 +15,25 @@ const Pokemon = class {
 const Cazador = class {
     constructor (puntuacion = 0, imagen) {
         this.puntuacion = puntuacion;
-        this.img = imagen;
+        this.img = cazadorImages;
     }
 }
 
 const Arquero = class extends Cazador {
     constructor () {
         super();
-        this.img = "assets/cazadores/cazador" + numAleatorio(1,5) + ".svg" ;
+        this.img = cazadorImages["cazador"+ numAleatorio(1,5)];
         this.flechas = numAleatorio(200,300);
     }
 
     atacar(Pokemon){
         
-        if(flechas > 0){
+        if(this.flechas > 0){
             if (Pokemon.life <= 0){
                 return true;
             }else{
                 let numeroFlechasAtaque = numAleatorio(10,20);
-                flechas -= numeroFlechasAtaque;
+                this.flechas -= numeroFlechasAtaque;
                 Pokemon.life -= numeroFlechasAtaque * 2;
                 this.puntuacion += numeroFlechasAtaque * 2;
                 if (Pokemon.life <=0){
@@ -47,7 +50,7 @@ const Arquero = class extends Cazador {
 const Luchador = class extends Cazador {
     constructor () {
         super();
-        this.img = "assets/cazadores/cazador" + numAleatorio(1,5) + ".svg" ;
+        this.img = cazadorImages["cazador"+ numAleatorio(1,5)];
         this.fuerza = numAleatorio(20,40);
     }
 
@@ -67,20 +70,71 @@ const Luchador = class extends Cazador {
     }
 }
 const botonPintar = document.querySelector('#botonPintar');
+const botonAtacar = document.querySelector('#botonAtacar');
+
+const arrayLuchadores = [];
+const arrayPokemons = [];
 
 function pintarCampoLucha(){
+    let numJugadores = numAleatorio(3,6);
 
-    let personaje = new Arquero();
-    console.log(personaje);
+    
 
-    let cazadores = document.querySelector(".cazadores")
-    alert("Empieza el juego");
-    const divLuchador = document.createElement('div');
-    divLuchador.className = "cazador";
-    divLuchador.innerHTML = '<img src="assets/cazadores/cazador1.svg"></img><span>Puntuación:' + personaje.puntuacion + "</span><span>Daño: "+ personaje.flechas +"</span>"
-    cazadores.appendChild(divLuchador);
+    for(let i = 1; i<= numJugadores; i++){
+        let tipoCazador = numAleatorio(1,2);
+        if (tipoCazador == 1){
+            //Se pinta un arquero
+            const personaje = new Arquero();
+            const cazadores = document.querySelector(".cazadores")
+            const divLuchador = document.createElement('div');
+            divLuchador.className = "cazador";
+            divLuchador.innerHTML = `<span>${personaje.constructor.name + " " +i }</span>
+                            <img src="${personaje.img}">
+                            <span>Puntuación: ${personaje.puntuacion}</span>
+                            <span>Flechas: ${personaje.flechas}</span>`
+            cazadores.appendChild(divLuchador);
+            arrayLuchadores.push(personaje);
+
+        }else{
+            const personaje = new Luchador();
+            const cazadores = document.querySelector(".cazadores")
+            const divLuchador = document.createElement('div');
+            divLuchador.className = "cazador";
+            divLuchador.innerHTML = `<span>${personaje.constructor.name + " " +i}</span>
+                                    <img src="${personaje.img}">
+                                    <span>Puntuación: ${personaje.puntuacion}</span>
+                                    <span>Fuerza: ${personaje.fuerza}</span>`
+            cazadores.appendChild(divLuchador);
+            arrayLuchadores.push(personaje);
+        }
+
+        //Pintar un pokemon por cazador
+        const pokemon = new Pokemon();
+        const pokemons = document.querySelector(".pokemons")
+        const divPokemon = document.createElement('div');
+        divPokemon.className = "pokemon";
+        divPokemon.innerHTML = `<span>${pokemon.constructor.name + " " +i}</span>
+                                <img src="${pokemon.img}">
+                                <span>Vida: ${pokemon.life}</span>`
+        pokemons.appendChild(divPokemon);
+        arrayPokemons.push(pokemon);
+    }
+
+
+    botonPintar.style.display = "none";
+    botonAtacar.style.display = "unset";
+}
+
+
+function atacar(){
+
+    console.log(arrayLuchadores);
+    console.log(arrayPokemons);
+    arrayLuchadores[0].atacar(arrayPokemons[0]);
+
 }
 
 
 botonPintar.addEventListener('click', pintarCampoLucha);
+botonAtacar.addEventListener('click', atacar);
 
